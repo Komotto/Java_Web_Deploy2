54a93b76bf677bee4ac080849dd5a8ae3762233f
Komotto <58824478+Komotto@users.noreply.github.com>
2025-12-10 16:44:01 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -62,31 +62,3 @@ public class Main {
         server.createContext("/", new RootHandler());
         server.setExecutor(null);
         server.start();
-
-        System.out.println("Servidor rodando em http://localhost:" + port + "/");
-    }
-
-    static class RootHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-
-            String html = """
-                    <html>
-                      <head><meta charset="UTF-8"><title>Olá Mundo</title></head>
-                      <body>
-                        <h1>Olá Mundo Web — Java Puro!</h1>
-                      </body>
-                    </html>
-                    """;
-
-            byte[] response = html.getBytes("UTF-8");
-
-            exchange.getResponseHeaders().add("Content-Type", "text/html; charset=utf-8");
-            exchange.sendResponseHeaders(200, response.length);
-
-            try (OutputStream os = exchange.getResponseBody()) {
-                os.write(response);
-            }
-        }
-    }
-}
