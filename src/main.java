f6993b6299bb674263084412017a4d74a6c8dcf4
Komotto <58824478+Komotto@users.noreply.github.com>
2025-12-10 13:06:21 -0300

    Create main.java

---
+++ src/main.java
@@ -0,0 +1,52 @@
+git init
+git add README.md
+git commit -m "teste1"
+git branch -M main
+git remote add origin https://github.com/Komotto/Java_Web_Deploy2.git
+git push -u origin main
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new RootHandler());
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Servidor rodando em http://localhost:" + port + "/");
+    }
+
+    static class RootHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+            String html = """
+                    <html>
+                      <head><meta charset="UTF-8"><title>Olá Mundo</title></head>
+                      <body>
+                        <h1>Olá Mundo Web — Java Puro!</h1>
+                      </body>
+                    </html>
+                    """;
+
+            byte[] response = html.getBytes("UTF-8");
+
+            exchange.getResponseHeaders().add("Content-Type", "text/html; charset=utf-8");
+            exchange.sendResponseHeaders(200, response.length);
+
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response);
+            }
+        }
+    }
+}
