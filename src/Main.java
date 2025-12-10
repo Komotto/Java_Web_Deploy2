51906db4cebbf4ff30deba9e375c281e2717b3c5
Komotto <58824478+Komotto@users.noreply.github.com>
2025-12-10 16:47:00 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
@@ -6,37 +5,35 @@ import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
         int port = 8080;
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
+        try {
+            logger.info("Iniciando servidor na porta " + port);
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
+            HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", port), 0);
+            server.createContext("/", new RootHandler());
+            server.setExecutor(null);
+            server.start();
 
-public class Main {
+            logger.info("Servidor rodando em http://localhost:" + port + "/");
 
-    public static void main(String[] args) throws IOException {
-        int port = 8080;
-
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/", new RootHandler());
-        server.setExecutor(null);
-        server.start();
-
-        System.out.println("Servidor rodando em http://localhost:" + port + "/");
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Erro ao iniciar o servidor: ", e);
+        }
     }
 
     static class RootHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
+            logger.info("Requisição recebida: " + exchange.getRequestURI());
 
             String html = """
                     <html>
@@ -54,11 +51,8 @@ public class Main {
 
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(response);
+                logger.info("Resposta enviada com sucesso.");
             }
         }
     }
 }
-
-        server.createContext("/", new RootHandler());
-        server.setExecutor(null);
-        server.start();
